cba5595749416304139eb39e5147bfc4a99cc4bb
Denitsa Velikova <deniitooou966@mail.bg>
2022-06-03 16:00:24 +0300

    C# Intro and Basic Syntax - Lab
    C# Intro and Basic Syntax - Lab

---
+++ C# Intro and Basic Syntax - Lab/01. Greeting/01. Greeting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01._Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
---
+++ C# Intro and Basic Syntax - Lab/03. Employee Data/03. Employee Data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Employee_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            short age = short.Parse(Console.ReadLine());
+            long id = long.Parse(Console.ReadLine());
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Employee ID: {0:D8}", id);
+            Console.WriteLine("Salary: {0:f2}", salary);
+        }
+    }
+}
