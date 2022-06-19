0c0a213f00f2cb9ebfee68b140d5989610d73533
Denitsa Velikova <deniitooou966@mail.bg>
2022-06-19 14:15:14 +0300

    Methods - Exercise
    Methods - Exercise

---
+++ Methods - Exercise/05. Remove Negatives and Reverse/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            numbers.RemoveAll(num => num < 0);
+
+            numbers.Reverse();
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+
+                return;
+            }
+        }
+    }
+}
