f6f9a6a170f7f2b91dd298f1d8e6774dcc721cb4
Denitsa Velikova <deniitooou966@mail.bg>
2022-06-24 00:28:43 +0300

    Mid Exams
    Mid Exams

---
+++ Mid Exam/01. Black Flag/01. Black Flag/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDays = int.Parse(Console.ReadLine());
+            double dailyPlunder = double.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double plunder = 0;
+
+            for (int day = 1; day <= totalDays; day++)
+            {
+                plunder += dailyPlunder;
+                if (day % 3 == 0)
+                {
+                    plunder += 0.5 * dailyPlunder;
+                }
+                if (day % 5 == 0)
+                {
+                    plunder *= 0.7;
+                }
+            }
+
+            if (plunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {plunder:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {plunder / expectedPlunder * 100:f2}% of the plunder.");
+            }
+        }
+    }
+}
