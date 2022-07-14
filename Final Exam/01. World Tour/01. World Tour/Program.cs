b749fa92c512551f387b2815ea58bb2192c20e77
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-14 16:50:56 +0300

    Final Exams
    Final Exams

---
+++ Final Exam/01. World Tour/01. World Tour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._World_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
