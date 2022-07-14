5c873bc57636ef673ad6938a5b80a073f1cdfab2
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-14 16:54:17 +0300

    Final Exam
    Final Exam

--- Final Exam/01. World Tour/01. World Tour/Program.cs
+++ Final Exam/01. World Tour/01. World Tour/Program.cs
@@ -6,7 +6,60 @@ namespace _01._World_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string stops = Console.ReadLine();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Travel")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(':');
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Add Stop":
+                        int index = int.Parse(tokens[1]);
+                        string newStop = tokens[2];
+
+                        if (index >= 0 && index < stops.Length)
+                        {
+                            stops = stops.Insert(index, newStop);
+                        }
+                        break;
+
+                    case "Remove Stop":
+                        int startIndex = int.Parse(tokens[1]);
+                        int endIndex = int.Parse(tokens[2]);
+
+                        if (startIndex >= 0
+                            && startIndex < stops.Length
+                            && endIndex >= 0
+                            && endIndex < stops.Length)
+                        {
+                            stops = stops.Remove(startIndex, endIndex - startIndex + 1);
+                        }
+                        break;
+
+                    case "Switch":
+                        string oldString = tokens[1];
+                        string newString = tokens[2];
+
+                        if (stops.Contains(oldString))
+                        {
+                            stops = stops.Replace(oldString, newString);
+                        }
+                        break;
+                }
+
+                Console.WriteLine(stops);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
         }
     }
 }
