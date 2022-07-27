4bbc2212218fea52285b9535f1ff245d97f88e32
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:27:16 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/04. Caesar Cipher/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            char[] inputChars = inputText.ToCharArray();
+
+            char[] outputChars = new char[inputChars.Length];
+
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                char result = Convert.ToChar((inputChars[i] + 3));
+                outputChars[i] = result;
+            }
+            Console.WriteLine(String.Join("", outputChars));
+        }
+    }
+}
