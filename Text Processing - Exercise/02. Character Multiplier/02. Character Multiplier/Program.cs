91c25b775c9f665ea8fa7fd1ee03ab1237e4085e
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:23:41 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/02. Character Multiplier/02. Character Multiplier/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputStrings = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            char[] firstString = inputStrings[0].ToCharArray();
+            char[] secondString = inputStrings[1].ToCharArray();
+
+            long totalSum = 0;
+            int maxLength = Math.Max(firstString.Length, secondString.Length);
+            int minLength = Math.Min(firstString.Length, secondString.Length);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                totalSum += firstString[i] * secondString[i];
+            }
+
+            if (maxLength == firstString.Length)
+            {
+                for (int i = minLength; i < maxLength; i++)
+                {
+                    totalSum += firstString[i];
+                }
+            }
+            else
+            {
+                for (int i = minLength; i < maxLength; i++)
+                {
+                    totalSum += secondString[i];
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
