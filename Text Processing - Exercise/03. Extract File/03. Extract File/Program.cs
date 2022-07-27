4ef4abb81914bb159a375f7eca06fa471f5e1018
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:25:15 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/03. Extract File/03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split("\\");
+
+            string[] fileAndText = path[path.Length - 1].Split('.');
+
+            string file = fileAndText[0];
+            string text = fileAndText[1];
+
+            Console.WriteLine($"File name: {file}");
+            Console.WriteLine($"File extension: {text}");
+        }
+    }
+}
