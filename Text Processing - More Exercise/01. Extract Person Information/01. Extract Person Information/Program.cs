15fa6e3f2adf8ce4a7200f2bf83928ba09952713
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:42:06 +0300

    Text Processing - More Exercise
    Text Processing - More Exercise

---
+++ Text Processing - More Exercise/01. Extract Person Information/01. Extract Person Information/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCnt = int.Parse(Console.ReadLine());
+
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < inputCnt; i++)
+            {
+                string inputString = Console.ReadLine();
+
+                string name = FindName(inputString);
+                string age = FindAge(inputString);
+
+                output.AppendLine($"{name} is {age} years old.");
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+
+        private static string FindAge(string inputString)
+        {
+            int startIndex = inputString.IndexOf('#');
+            int endIndex = inputString.IndexOf('*');
+
+            string age = inputString.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+            return age;
+        }
+
+        private static string FindName(string inputString)
+        {
+            int startIndex = inputString.IndexOf('@');
+            int endIndex = inputString.IndexOf('|');
+
+            string name = inputString.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+            return name;
+        }
+    }
+}
