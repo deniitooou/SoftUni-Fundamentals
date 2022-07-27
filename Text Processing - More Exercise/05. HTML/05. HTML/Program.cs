293f14f7f365417f17416bf560c3fb76eaf772db
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:53:56 +0300

    Text Processing - More Exercise
    Text Processing - More Exercise

---
+++ Text Processing - More Exercise/05. HTML/05. HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _05._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string article = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+            output.AppendLine("<h1>");
+            output.AppendLine($"    {title}");
+            output.AppendLine("</h1>");
+
+            output.AppendLine("<article>");
+            output.AppendLine($"    {article}");
+            output.AppendLine("</article>");
+
+            string inputComment = Console.ReadLine();
+            while (inputComment != "end of comments")
+            {
+                output.AppendLine("<div>");
+                output.AppendLine($"    {inputComment}");
+                output.AppendLine("</div>");
+
+                inputComment = Console.ReadLine();
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
