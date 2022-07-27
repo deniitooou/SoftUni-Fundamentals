1bc08bae68c9c24f0170f4216cb17edc69f25a37
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 17:43:19 +0300

    Regular Expressions - More Exercise
    Regular Expressions - More Exercise

---
+++ Regular Expressions - More Exercise/1. Winning Ticket/1. Winning Ticket/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _1._Winning_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allTickets = Console.ReadLine()
+              .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < allTickets.Length; i++)
+            {
+                string ticket = allTickets[i];
+
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                string leftSide = String.Join("", ticket.Take(10));
+                string rightSide = String.Join("", ticket.Skip(10));
+
+                string regex = @"([@#$^])\1{5,9}";
+                MatchCollection matchLeft = Regex.Matches(leftSide, regex);
+                MatchCollection matchRight = Regex.Matches(rightSide, regex);
+
+                if ((matchLeft.Count > 0 && matchRight.Count > 0) &&
+                    (matchLeft[0].Value.Contains(matchRight[0].Value) ||
+                    matchRight[0].Value.Contains(matchLeft[0].Value)))
+                {
+                    char winningSymbol = matchLeft[0].Value[0];
+                    int winningLength = Math.Min(matchLeft[0].Value.Length, matchRight[0].Value.Length);
+
+                    string output = $"ticket \"{ticket}\" - {winningLength}{winningSymbol}";
+                    if (winningLength == 10) output += " Jackpot!";
+                    Console.WriteLine(output);
+                }
+                else
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+            }
+        }
+    }
+}
+
---
+++ Regular Expressions - More Exercise/2. Rage Quit/2. Rage Quit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2._Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+
+            MatchCollection matchesByPattern = Regex.Matches(input, @"(?<string>[^\d]+)(?<number>[\d]+)");
+            StringBuilder resultString = new StringBuilder();
+
+            foreach (Match match in matchesByPattern)
+                resultString.Append(GetRepeatedText(match));
+
+            char[] uniqueSymbols = GetUniqueSymbols(resultString.ToString());
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols.Length}");
+            Console.WriteLine($"{resultString}");
+        }
+
+        static string GetRepeatedText(Match match)
+        {
+            StringBuilder sb = new StringBuilder();
+            string rageText = match.Groups["string"].Value;
+            int numberOfRepeats = int.Parse(match.Groups["number"].Value);
+
+            for (int i = 0; i < numberOfRepeats; i++)
+                sb.Append(rageText);
+
+            return sb.ToString();
+        }
+
+        static char[] GetUniqueSymbols(string result)
+        {
+            var uniqueSymbolsList = new List<char>();
+            foreach (char c in result)
+            {
+                if (c >= '0' && c <= '9') continue;
+                if (!uniqueSymbolsList.Contains(c))
+                    uniqueSymbolsList.Add(c);
+            }
+            return uniqueSymbolsList.ToArray();
+        }
+    }
+}
