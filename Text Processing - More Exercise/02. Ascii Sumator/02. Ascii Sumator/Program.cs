7d7093d16215b882b690b7f6cc53b3d5723db933
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:43:27 +0300

    Text Processing - More Exercise
    Text Processing - More Exercise

---
+++ Text Processing - More Exercise/02. Ascii Sumator/02. Ascii Sumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startSymbol = char.Parse(Console.ReadLine());
+            char endSymbol = char.Parse(Console.ReadLine());
+            string inputString = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] > startSymbol && inputString[i] < endSymbol)
+                {
+                    sum += inputString[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
