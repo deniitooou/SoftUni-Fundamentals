0333ab8f58d227ee8a45305d3a64a25ab9bac492
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:31:23 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/07. String Explosion/07. String Explosion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int power = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (power > 0 && inputString[i] != '>')
+                {
+                    inputString = inputString.Remove(i, 1);
+                    power--;
+                    i--;
+                }
+                else if (inputString[i] == '>')
+                {
+                    power += int.Parse(inputString[i + 1].ToString());
+                }
+            }
+            Console.WriteLine(inputString);
+        }
+    }
+}
