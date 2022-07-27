356e004566762a54b513466468f21e8f21add397
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:28:27 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/05. Multiply Big Number/05. Multiply Big Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNum = BigInteger.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            BigInteger result = firstNum * secondNum;
+            Console.WriteLine(result);
+        }
+    }
+}
