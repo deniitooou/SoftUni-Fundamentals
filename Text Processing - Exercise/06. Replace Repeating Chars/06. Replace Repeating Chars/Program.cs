104f1735e15a3075001c27783e6a93273becb9b5
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:29:47 +0300

    Text Processing - Exercise
    Text Processing - Exercise

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i + 1] == input[i])
+                {
+                    input = input.Remove(i + 1, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
