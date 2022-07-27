5f6d103f8647e48d346ea874669adfd4596c5e58
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 17:49:49 +0300

    Regular Expressions - More Exercise
    Regular Expressions - More Exercise

--- Regular Expressions - More Exercise/3. Post Office/3. Post Office/Program.cs
+++ Regular Expressions - More Exercise/3. Post Office/3. Post Office/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _3._Post_Office
 {
@@ -6,7 +9,74 @@ namespace _3._Post_Office
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split('|');
+            char[] capitalLetters = GetCapitalLetters(input[0]);
+            Dictionary<char, int> wordLengthsByFirstLetter = GetWordLengths(input[1], capitalLetters);
+            List<string> extractedWords = ExtractValidWords(input[2], wordLengthsByFirstLetter);
+
+            for (int i = 0; i < capitalLetters.Length; i++)
+            {
+                Console.WriteLine(extractedWords.Find(x => x[0] == capitalLetters[i]));
+            }
+        }
+
+        private static List<string> ExtractValidWords(string inputText, Dictionary<char, int> wordLengthsByFirstLetter)
+        {
+            string[] allWords = inputText.Split();
+            List<string> validWords = new List<string>();
+
+            foreach (string word in allWords)
+            {
+                char firstLetter = word[0];
+
+                if (wordLengthsByFirstLetter.ContainsKey(firstLetter)
+                    && wordLengthsByFirstLetter[firstLetter] == word.Length)
+                {
+                    validWords.Add(word);
+                }
+            }
+
+            return validWords;
+        }
+
+        private static Dictionary<char, int> GetWordLengths(string inputText, char[] capitalLetters)
+        {
+            var wordLengthsByFirstLetter = new Dictionary<char, int>();
+
+            string regex = @"(?<ascii>[\d]{2}):(?<length>[\d]{2})";
+            MatchCollection matches = Regex.Matches(inputText, regex);
+
+            foreach (Match match in matches)
+            {
+                char asciiSymbol = (char)int.Parse(match.Groups["ascii"].Value);
+                int length = 1 + int.Parse(match.Groups["length"].Value);
+
+                if (capitalLetters.Contains(asciiSymbol))
+                {
+                    if (wordLengthsByFirstLetter.ContainsKey(asciiSymbol))
+                        wordLengthsByFirstLetter[asciiSymbol] = length;
+
+                    else
+                        wordLengthsByFirstLetter.Add(asciiSymbol, length);
+                }
+            }
+
+            return wordLengthsByFirstLetter;
+        }
+
+        private static char[] GetCapitalLetters(string inputText)
+        {
+            string regex = @"([#$%*&])(?<capitalChars>[A-Z]+)\1";
+
+            MatchCollection match = Regex.Matches(inputText, regex);
+            string regexResult = String.Empty;
+
+            for (int i = 0; i < match.Count; i++)
+            {
+                regexResult += match[i].Groups["capitalChars"].Value;
+            }
+
+            return regexResult.ToCharArray();
         }
     }
 }
---
+++ Regular Expressions - More Exercise/4. Santa's Secret Helper/4. Santa's Secret Helper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _4._Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            var goodChildren = new List<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string message = GetDecodedInput(input, key);
+                string regex = @"[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]";
+                MatchCollection matchInfo = Regex.Matches(message, regex);
+
+                if (matchInfo.Count == 0)
+                {
+                    continue;
+                }
+
+                string childName = matchInfo[0].Groups["name"].Value;
+                string childBehaviour = matchInfo[0].Groups["behaviour"].Value;
+
+                if (childBehaviour == "G")
+                {
+                    goodChildren.Add(childName);
+                }
+            }
+
+            Console.WriteLine(String.Join("\n", goodChildren));
+        }
+
+        static string GetDecodedInput(string input, int key)
+        {
+            char[] decodedChars = input
+                .ToCharArray()
+                .Select(x => (char)(x - key))
+                .ToArray();
+
+            return String.Join("", decodedChars);
+        }
+    }
+}
