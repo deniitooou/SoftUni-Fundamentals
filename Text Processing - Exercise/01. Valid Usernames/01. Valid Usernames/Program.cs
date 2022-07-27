c3680b711daaa8d692509cc604a6606304b2ff6b
Denitsa Velikova <deniitooou966@mail.bg>
2022-07-27 15:20:46 +0300

    Text Processing - Exercise
    Text Processing - Exercise

--- Text Processing - Exercise/01. Valid Usernames/01. Valid Usernames/Program.cs
+++ Text Processing - Exercise/01. Valid Usernames/01. Valid Usernames/Program.cs
@@ -6,7 +6,30 @@ namespace _01._Valid_Usernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] passwords = Console.ReadLine().Split(", ");
+
+            foreach (var password in passwords)
+            {
+                if (password.Length > 3 && password.Length < 16)
+                {
+                    bool isValid = true;
+                    foreach (char ch in password)
+                    {
+                        if (!char.IsLetterOrDigit(ch)
+                            && ch != '-'
+                            && ch != '_')
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        Console.WriteLine(password);
+                    }
+                }
+            }
         }
     }
 }
